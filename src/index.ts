dfe87d3c205058f39a88b8c7e7abc7a1f0251c78
taoqf <tao_qiufeng@126.com>
2020-04-02 17:50:25 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,31 @@
+import global from '@mmstudio/global';
 
-export default function awx4(param1: string) {
+/**
+ * 下载在线文档并打开
+ * @param url [string] 文件路径或id
+ */
+export default function open_document(url: string) {
+	if (!/^https?/i.test(url)) {
+		const host = global('host', 'http://127.0.0.1:8889');
+		url = `${host}/fsweb/getfile?id=${url}`;
+	}
+	return new Promise<boolean>((resolve) => {
+		wx.downloadFile({
+			url,
+			success(res) {
+				wx.openDocument({
+					filePath: res.tempFilePath,
+					success() {
+						resolve(true);
+					},
+					fail() {
+						resolve(false);
+					}
+				});
+			},
+			fail() {
+				resolve(false);
+			}
+		});
+	});
 }
